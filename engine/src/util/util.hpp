78d5fc022c8495408da8af97644048c232b6bc70
bakje <59623580+bakjedev@users.noreply.github.com>
2025-11-02 13:20:20 +0100

    Make build workflow run on every commit

--- engine/src/util/util.hpp
+++ engine/src/util/util.hpp
@@ -4,6 +4,7 @@
 
 namespace Util {
 
+// basically std::print from c++23
 template <class... Args>
 void print(std::format_string<Args...> fmt, Args&&... args) {
   std::cout << std::format(fmt, std::forward<Args>(args)...);
