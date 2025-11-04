fd007c07cfa05dacdfb7e776afb508c861550591
bakje <59623580+bakjedev@users.noreply.github.com>
2025-11-04 21:27:17 +0100

    Rework resource pool to do everything, add resource handle

--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -1,18 +1,11 @@
 #include "core/engine.hpp"
 #include "input/input.hpp"
 #include "input/input_enums.hpp"
-#include "resource/resource_pool.hpp"
+#include "resource/resource.hpp"
 #include "util/util.hpp"
 
 struct TestResource {
   int number = 44;
-
-  TestResource() { Util::println("CREATED TEST RESOURCE"); }
-  TestResource(int value) : number(value) {
-    Util::println("CREATED TEST RESOURCE with {}", number);
-  }
-
-  ~TestResource() { Util::println("DESTROYED TEST RESOURCE"); }
 };
 
 struct TestResourceLoader {
@@ -22,11 +15,14 @@ struct TestResourceLoader {
 struct RuntimeApplication {
   void init() {
     ResourcePool<TestResource> pool;
-    auto handle = pool.allocate();
-    pool.registerKey("test", handle.index);
-    pool.load(handle, TestResourceLoader{}, 37);
-    pool.release(handle);
-    Util::print("forty five {}\n", 45);
+
+    auto test = pool.load("test", TestResourceLoader{}, 22);
+
+    Util::println("test {}", test->number);
+
+    auto testTWO = pool.load("test", TestResourceLoader{}, 16);
+
+    Util::println("testTWO {}", testTWO->number);
   }
 
   void update(float) {
