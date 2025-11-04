dc95d7cd49775277e9cf91564023d9ecf1d5bfce
bakje <59623580+bakjedev@users.noreply.github.com>
2025-11-04 19:00:17 +0100

    Add resource pool

--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -1,10 +1,33 @@
 #include "core/engine.hpp"
 #include "input/input.hpp"
 #include "input/input_enums.hpp"
+#include "resource/resource_pool.hpp"
 #include "util/util.hpp"
 
+struct TestResource {
+  int number = 44;
+
+  TestResource() { Util::println("CREATED TEST RESOURCE"); }
+  TestResource(int value) : number(value) {
+    Util::println("CREATED TEST RESOURCE with {}", number);
+  }
+
+  ~TestResource() { Util::println("DESTROYED TEST RESOURCE"); }
+};
+
+struct TestResourceLoader {
+  TestResource operator()(int value) const { return TestResource{value}; }
+};
+
 struct RuntimeApplication {
-  void init() { Util::print("forty five {}\n", 45); }
+  void init() {
+    ResourcePool<TestResource> pool;
+    auto handle = pool.allocate();
+    pool.registerKey("test", handle.index);
+    pool.load(handle, TestResourceLoader{}, 37);
+    pool.release(handle);
+    Util::print("forty five {}\n", 45);
+  }
 
   void update(float) {
     auto& input = engine.getInput();
