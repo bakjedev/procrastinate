8ea6425870c8c7fe5fd0ca3d22132f8841ed7d24
bakje <59623580+bakjedev@users.noreply.github.com>
2025-11-16 10:37:17 +0100

    Switch to slang for shaders

--- external/shaders.cmake
+++ external/shaders.cmake
@@ -1,14 +1,18 @@
-find_program(GLSLC_EXECUTABLE
-	NAMES glslc
+find_program(SLANGC_EXECUTABLE
+	NAMES slangc
 )
 
-if(NOT GLSLC_EXECUTABLE)
-    message(FATAL_ERROR "glslc compiler not found")
+if(NOT SLANGC_EXECUTABLE)
+    message(FATAL_ERROR "slangc compiler not found")
 endif()
 
-message(STATUS "Found glslc: ${GLSLC_EXECUTABLE}")
+message(STATUS "Found slangc: ${SLANGC_EXECUTABLE}")
 
-function(compile_shaders SHADER_DIR OUTPUT_DIR)
+function(compile_shaders TARGET_NAME SHADER_DIR OUTPUT_DIR)
+	if(NOT TARGET_NAME)
+		message(FATAL_ERROR "no TARGET_NAME argument")
+	endif()	
+
 	if(NOT SHADER_DIR)
 		message(FATAL_ERROR "no SHADER_DIR argument")
 	endif()
@@ -20,17 +24,17 @@ function(compile_shaders SHADER_DIR OUTPUT_DIR)
 	file(MAKE_DIRECTORY ${OUTPUT_DIR})
 
 	file(GLOB_RECURSE SHADER_FILES
-		"${SHADER_DIR}/*.vert"
-		"${SHADER_DIR}/*.frag"
-		"${SHADER_DIR}/*.comp"
+		"${SHADER_DIR}/*.slang"
 	)
 
 	set(SPIRV_FILES "")
 
 	foreach(SHADER_FILE ${SHADER_FILES})
 		file(RELATIVE_PATH REL_SHADER_PATH ${SHADER_DIR} ${SHADER_FILE})
-		get_filename_component(SHADER_NAME ${REL_SHADER_PATH} NAME)
+		get_filename_component(SHADER_NAME ${REL_SHADER_PATH} NAME_WLE)
 		get_filename_component(SHADER_SUBDIR ${REL_SHADER_PATH} DIRECTORY)
+
+		message("found shader with ${SHADER_FILE}, with rel path ${REL_SHADER_PATH}")
 
 		if(SHADER_SUBDIR)
 			file(MAKE_DIRECTORY "${OUTPUT_DIR}/${SHADER_SUBDIR}")
@@ -38,10 +42,14 @@ function(compile_shaders SHADER_DIR OUTPUT_DIR)
 		else()
 			set(SPIRV_FILE "${OUTPUT_DIR}/${SHADER_NAME}.spv")
 		endif()
+		message("going to compile it to ${SPIRV_FILE}")
 
 		add_custom_command(
 			OUTPUT ${SPIRV_FILE}
-			COMMAND ${GLSLC_EXECUTABLE} -o ${SPIRV_FILE} ${SHADER_FILE}
+			COMMAND ${SLANGC_EXECUTABLE}
+			-target spirv
+			-o ${SPIRV_FILE}
+			${SHADER_FILE}
 			DEPENDS ${SHADER_FILE}
 			COMMENT "Compiling ${REL_SHADER_PATH}"
 			VERBATIM
@@ -50,5 +58,8 @@ function(compile_shaders SHADER_DIR OUTPUT_DIR)
 		list(APPEND SPIRV_FILES ${SPIRV_FILE})
 	endforeach()
 
-	add_custom_target(shaders ALL DEPENDS ${SPIRV_FILES})
+	add_custom_target(${TARGET_NAME} ALL
+		DEPENDS ${SPIRV_FILES}
+		COMMENT "Compiling all shaders"
+	)
 endfunction()
