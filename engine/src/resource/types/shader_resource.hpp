a8966906da23b07cabed36b07b414fc5b1488a04
bakjedev <bakje.mail@gmail.com>
2026-02-28 13:30:31 +0100

    Move shader loading in to shader loader (duh)

--- engine/src/resource/types/shader_resource.hpp
+++ engine/src/resource/types/shader_resource.hpp
@@ -9,10 +9,18 @@ struct ShaderResource
 {
   std::vector<uint32_t> code;
 
-  explicit ShaderResource(const std::string& path) { code = files::ReadBinaryFile(path).value(); }
+  explicit ShaderResource(std::vector<uint32_t> data) : code(std::move(data)) {}
 };
 
 struct ShaderResourceLoader
 {
-  ShaderResource operator()(const std::string& path) const { return ShaderResource{path}; }
+  ShaderResource operator()(const std::string& path) const
+  {
+    auto data = files::ReadBinaryFile(path);
+    if (!data)
+    {
+      throw std::runtime_error("Failed to load shader: " + path);
+    }
+    return ShaderResource{std::move(*data)};
+  }
 };
